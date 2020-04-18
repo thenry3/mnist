191edd93ea13d5361b007c947a1606e0a684f8d9
Henry <henrymtrinh14@gmail.com>
2020-04-17 23:50:14 -0700

    asfd

---
+++ main.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import numpy as np
+
+class MnistNet(nn.Module):
+    def __init__(self):
+        super(MnistNet, self).__init__()
+
+        self.conv1 = nn.Conv2d
+
+x = np.random.rand(2, 3, 4)
+print(x)
+print(x[1, : , 3])
