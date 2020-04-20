6a8e1505c83ee353a81bfef542776bff885b4e38
Henry <henrymtrinh14@gmail.com>
2020-04-19 22:57:45 -0700

    fix batch

--- lstm.py
+++ lstm.py
@@ -3,9 +3,9 @@ import torch
 
 # n -> batch size, l -> sequence length, c -> feature dim, d -> output dim
 def do_lstm(n, l, c, d):
-    lstm = nn.LSTM(c, d//2, num_layers=2, bidirectional=True)
+    lstm = nn.LSTM(c, d//2, num_layers=2, bidirectional=True, batch_first=True)
     given = torch.randn(n, l, c)
-    hidden = (torch.randn(4, l, d//2), torch.randn(4, l, d//2))
+    hidden = (torch.randn(4, n, d//2), torch.randn(4, n, d//2))
     output, hidden = lstm(given, hidden)
     return output
 
