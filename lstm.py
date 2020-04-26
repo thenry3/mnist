28a0885d0d2521bf732518ecec44e7b4bc2c6e3c
Henry <henrymtrinh14@gmail.com>
2020-04-26 01:13:48 -0700

    lmao is this right?

--- lstm.py
+++ lstm.py
@@ -1,12 +1,26 @@
 import torch.nn as nn
 import torch
 
-# n -> batch size, l -> sequence length, c -> feature dim, d -> output dim
+
 def do_lstm(n, l, c, d):
+    """
+    n -> batch size, l -> sequence length, c -> feature dim, d -> output dim
+    """
     lstm = nn.LSTM(c, d//2, num_layers=2, bidirectional=True, batch_first=True)
     given = torch.randn(n, l, c)
     hidden = (torch.randn(4, n, d//2), torch.randn(4, n, d//2))
     output, hidden = lstm(given, hidden)
     return output
 
-print(do_lstm(4, 5, 5, 10))
+
+def lstm(n, l, c, d):
+    lstm = nn.LSTM(c, d//2, num_layers=2, bidirectional=True)
+    given = torch.randn(l, n, c)
+    hidden = (torch.randn(4, n, d//2), torch.randn(4, n, d//2))
+    output, hidden = lstm(given, hidden)
+    print(output)
+    return torch.stack([output[:, i] for i in range(n)])
+
+
+# print(do_lstm(4, 5, 5, 10))
+print(lstm(4, 5, 5, 10))
